e0b5d12d10ece9efecdecc1f807fbbdb56854053
igorbrasileiro <brasileiro456@gmail.com>
2022-07-15 18:19:48 -0300

    Add more button at Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import NewsList from '../components/NewsList'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import NewsList from "../components/NewsList";
 
 export default function Home() {
   return (
@@ -10,7 +10,10 @@ export default function Home() {
       </Head>
       <section>
         <NewsList />
+        <div className="block h-[10px]" />
+        <button className="text-[10pt] leading-[normal] pl-[31px]">More</button>
+        <div className="block h-[10px]" />
       </section>
     </Layout>
-  )
+  );
 }
