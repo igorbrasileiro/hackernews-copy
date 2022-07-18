23b06268af27065a33bcb2df530fba7a337b1019
igorbrasileiro <brasileiro456@gmail.com>
2022-07-17 21:58:46 -0300

    Add hideNews, showIndex, showAuthor and showComments to NewsRow

--- components/NewsList.tsx
+++ components/NewsList.tsx
@@ -1,22 +1,30 @@
 import { NewsType, useNewsData } from "../sdk/useNewsData";
-import NewRow from "./NewsRow";
+import NewsRow from "./NewsRow";
 
 interface Props {
   newsType: NewsType
+  hideNews?: boolean
+  showIndex?: boolean
+  showAuthor?: boolean
+  showComments?: boolean
 }
 
-function NewsList({ newsType }: Props) {
+function NewsList({ newsType, showIndex, showAuthor, showComments, hideNews }: Props) {
   const newsData = useNewsData(newsType);
 
   return (
     <>
       {newsData.map((newsItemData, index) => {
         return (
-          <NewRow
+          <NewsRow
             key={index}
             {...newsItemData}
             isLast={index === newsData.length - 1}
             index={index + 1}
+            hideNews={hideNews}
+            showIndex={showIndex}
+            showAuthor={showAuthor}
+            showComments={showComments}
           />
         );
       })}
--- components/NewsRow.tsx
+++ components/NewsRow.tsx
@@ -7,6 +7,10 @@ interface Props extends EnhancedNewItemData {
   index?: number;
   isLast: boolean;
   isComment?: boolean;
+  hideNews?: boolean;
+  showIndex?: boolean;
+  showAuthor?: boolean;
+  showComments?: boolean;
 }
 
 function NewsRow({
@@ -21,6 +25,10 @@ function NewsRow({
   index,
   isLast,
   isComment,
+  hideNews = true,
+  showIndex = true,
+  showAuthor = true,
+  showComments = true,
 }: Props) {
   return (
     <div
@@ -30,20 +38,23 @@ function NewsRow({
       }`}
     >
       {/* TODO: Using fixed width. fix it */}
-      <div
-        className={`flex justify-end items-center ${
-          !isComment ? "w-[33px]" : ""
-        } h-[19px]`}
-      >
-        {!isComment && <span>{index}.</span>}
-        <span className="pl-[2px] cursor-pointer">
-          <img src={UpArrow.src} width={10} height={10} />
-        </span>
-      </div>
+      {showIndex && (
+        <div
+          className={`flex justify-end items-center ${
+            !isComment ? "w-[33px]" : ""
+          } h-[19px]`}
+        >
+          {!isComment && <span>{index}.</span>}
+          <span className="pl-[2px] cursor-pointer">
+            <img src={UpArrow.src} width={10} height={10} />
+          </span>
+        </div>
+      )}
+      {!showIndex && <div className="pl-[12px]" />}
 
-      <div className={`flex flex-col ${isComment ? 'pl-1' : 'pl-[2px]'}`}>
+      <div className={`flex flex-col ${isComment ? "pl-1" : "pl-[2px]"}`}>
         {/* Title Row */}
-        <div className={`h-[19px] pt-[3px] ${isComment ? 'mb-[4px]' : 'mb-[1px]'}`}>
+        <div className={`h-[19px] pt-[3px] ${isComment ? "mb-1" : "mb-[1px]"}`}>
           <a className="text-black visited:text-[#828282]" href={url}>
             {title}
           </a>
@@ -58,31 +69,40 @@ function NewsRow({
 
         {/* Info Row */}
         <div className="text-[7pt]">
-          {score} points by <span className="hover:underline">{by}</span>{" "}
-          <span
-            className="hover:underline cursor-pointer"
-            title={new Date(time).toISOString()}
-          >
-            {timeAgo(time * 1_000)}
-          </span>
-          {" | "}
-          <span className="hover:underline cursor-pointer">hide</span>
-          {" | "}
-
-          {isComment && (
-            <span className="hover:underline cursor-pointer">past</span>            
+          {showAuthor && (
+            <>
+              {score} points by <span className="hover:underline">{by}</span>{" "}
+            </>
           )}
-          {isComment && " | "}
-          {isComment && (
-            <span className="hover:underline cursor-pointer">favorite</span>            
-          )}
-          {isComment && " | "}
 
           <Link href={`/item/${id}`}>
-            <a className="hover:underline cursor-pointer">
-              {descendants} comments
+            <a className="hover:underline" title={new Date(time).toISOString()}>
+              {timeAgo(time * 1_000)}
             </a>
           </Link>
+
+          {hideNews && " | "}
+          {hideNews && (
+            <span className="hover:underline cursor-pointer">hide</span>
+          )}
+
+          {isComment && " | "}
+          {isComment && (
+            <span className="hover:underline cursor-pointer">past</span>
+          )}
+          {isComment && " | "}
+          {isComment && (
+            <span className="hover:underline cursor-pointer">favorite</span>
+          )}
+
+          {showComments && " | "}
+          {showComments && (
+            <Link href={`/item/${id}`}>
+              <a className="hover:underline cursor-pointer">
+                {descendants} comments
+              </a>
+            </Link>
+          )}
         </div>
       </div>
     </div>
