a483726f54e7555759a3db6b9110aa243e739aea
igorbrasileiro <brasileiro456@gmail.com>
2022-07-19 12:58:40 -0300

    Add Past and New Comments empty pages

---
+++ pages/front.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+
+function NewComments() {
+  return (
+    <Layout>
+      <Head>
+        <title>Past | {siteTitle}</title>
+      </Head>
+      <section>
+        TODO: Find API to get past posts
+        {/* https://hackernews.api-docs.io/v0/overview/introduction */}
+        {/* https://github.com/HackerNews/API */}
+        <div className="block h-[10px]" />
+        <button className="text-[10pt] leading-[normal] pl-[34px]">More</button>
+        <div className="block h-[10px]" />
+      </section>
+    </Layout>
+  );
+}
+
+
+export default NewComments
